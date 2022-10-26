1b4a33af9a2311c6e5d2c482d2149bd4185838b0
Geoff Schmit <gcschmit@gmail.com>
2022-10-25 20:07:29 -0500

    add notes on epsilon and string comparison

--- Conditions.java
+++ Conditions.java
@@ -79,4 +79,102 @@ public class Conditions
             System.out.println("coin is tails!");
             System.out.println("better luck next time...");
     }
+    
+    public static boolean doublesAreEqual(double num1, double num2)
+    {
+        /*
+         * If we use the equality operator (==) for doubles, it will only return true
+         *      if all binary bits match.
+         *  For "equal" numbers, this is probably not the case due to floating-point rounding
+         *      and, therefore, not what we want.
+         *   We will check if they are "close enough" (i.e., epsilon value).
+         */
+        final double EPSILON = 1e-6;
+        
+        if(Math.abs(num1 - num2) < EPSILON)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        // don't have to use an if statement
+        //return (Math.abs(num1 - num2) < EPSILON);
+    }
+    
+    public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter two words: ");
+        String str1 = s.next();
+        String str2 = s.next();
+        
+        /*
+         * The equality operator (==) returns true if the two variables (e.g., str1 and
+         *      str2) contain the same value. For variables of a class type, including String,
+         *      this means that they contain the same reference. That is, they refer to the
+         *      same object in memory, not that the two strings have the same sequence
+         *      of characters.
+         */
+        if(str1 == str2)
+        {
+            System.out.println("string references are equal");
+        }
+        else
+        {
+            System.out.println("string references are not equal");
+        }
+        
+        /*
+         * The equals method returns true if the two objects referenced by the variables
+         *      are "equal". What "equals" means is defined by the class. For strings,
+         *      it means that the two objects have the same sequence of characters.
+         */
+        if(str1.equals(str2))
+        {
+            System.out.println("strings are equal");
+        }
+        else
+        {
+            System.out.println("strings are not equal");
+        }
+        
+        /*
+         * If we want to check if strings are not equal, we use the logical complement
+         *      operator (i.e., !).
+         */
+        if( ! str1.equals(str2))
+        {
+            System.out.println("strings are NOT equal");
+        }
+        
+        /*
+         * We will determine which string comes first lexicographically using the
+         *      compareTo method of the String class.
+         *      
+         *  compareTo return an int value:
+         *      0:      if the strings are equal (same sequence of characters)
+         *      <0:     if str1 < str2 lexicographically
+         *      >0:     if str1 > str2 lexicographically
+         */
+        int result = str1.compareTo(str2);
+        
+        if(result < 0)
+        {
+            String firstStr = str1;
+        }
+        else if(result > 0)
+        {
+            String firstStr = str2;
+        }
+    }
+    
+    
 }
+
+
+
+
+
