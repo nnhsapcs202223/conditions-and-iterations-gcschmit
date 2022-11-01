c48ed0077953269e47d5c7790e8661ba1f9619a0
Geoff Schmit <gcschmit@gmail.com>
2022-11-01 14:19:57 -0500

    add notes on do-while and nested loops

--- Iterations.java
+++ Iterations.java
@@ -92,6 +92,111 @@ public class Iterations
         
         System.out.println("final value of count: " + count);
     }
+    
+    public static void offByOne()
+    {
+        /*
+         * The infamous off-by-one error is common with for loops
+         *      executing one too many or one too few times.
+         *      
+         *  Carefully ask:  Should the initial value start at 0 or 1?
+         *                  Should the condition be < or <= ?
+         *                  
+         *      "Think! Don't compile and try at random!"
+         *      
+         *  By convention, for simple for loops, we start at 0 and we use < operator.
+         */
+        
+        // we want to print five "*"
+        for(int i = 0;                                          // initialization
+                i <= 5; // should be i < 5                      // condition
+                i++)                                            // update the loop variable
+        {
+            System.out.println("*");                            // body
+        }
+        
+        System.out.println("done");
+    }
+    
+    public static void doExample()
+    {
+        /*
+         * do loop (do-while loop):
+         * 
+         *      1. executes teh body of the loop
+         *      2. evaluates the condition:
+         *          if true, executes the body of the loop again
+         *          if false, continues execution after the loop
+         */
+        
+        int count = 1;                                      // initialization
+        
+        do
+        {
+            System.out.println(count);                      // body
+            
+            count++;                                        // update the loop variable
+        }
+        while(count <= 5);                                  // condition
+        
+        System.out.println("done");
+    }
+    
+    public static int sum()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            /*
+             * Sentinel variable / value
+             *  Value (e.g., -1) used to terminate a loop.
+             *  It is often entered by a user.
+             */
+            value = s.nextInt();
+            
+            if(value != -1)
+            {
+                // this was a bug! sum was 1 less than it should be
+                //      before the if statement was added
+                sum += value;
+            }
+        }
+        while(value != -1);
+        
+        return sum;
+    }
+    
+    public static void loop1()
+    {
+        for(int i = 1; i <= 2; i++)
+        {
+            for(int j = 1; j <= 4; j++)
+            {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+    
+    public static void loop2()
+    {
+        int i = 1;
+        while(i <= 2)
+        {
+            int j = 1;
+            while(j <= 4)
+            {
+                System.out.println(i + " " + j);
+                j++;
+            }
+            i++;
+        }
+    }
 }
 
 
